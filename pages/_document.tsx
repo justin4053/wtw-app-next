d3c9a9cb78e4864b0b915b1a79bd361551ef498f
justin <jusin4053@gmail.com>
2022-06-14 15:15:42 +0800

    adjust

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,11 @@
-import Document from "next/document"
-import { ServerStyleSheet } from "styled-components"
+import Document, { DocumentContext } from "next/document"
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()]
-      }
-    } finally {
-      sheet.seal()
-    }
+    return initialProps
   }
 }
+
+export default MyDocument
